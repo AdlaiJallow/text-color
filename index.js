caf841164bc8b9293b8e36e43e917b40d4cbc0d8
AdlaiJallow <94768418+AdlaiJallow@users.noreply.github.com>
2024-01-01 23:39:19 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let bod = document.getElementById("bodd")
+
+function red() {
+  bod.style.color = "red";
+}
+
+function blue() {
+  bod.style.color = "blue";
+}
+
+function green() {
+  bod.style.color = "green";
+}
+
+function yellow() {
+  bod.style.color = "yellow";
+}
+
+function black() {
+  bod.style.color = "black";
+}
